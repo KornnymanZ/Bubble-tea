83f6a86d65d0e56ba418a895c810540b946355bf
KornnymanZ <157417122+KornnymanZ@users.noreply.github.com>
2024-03-27 02:10:25 +0700

    Add files via upload

--- src/components/Pages/Profilepage.jsx
+++ src/components/Pages/Profilepage.jsx
@@ -8,7 +8,7 @@ export default function ProfilePage() {
       <h1>{userProfile?.username}</h1>
       <img
         src='https://img.freepik.com/premium-vector/anonymous-user-circle-icon-vector-illustration-flat-style-with-long-shadow_520826-1931.jpg'
-        style={{ width: '100%' }}
+        style={{ width: '20%' }}
       />
       <p className='title'>Email : {userProfile?.email}</p>
       <p>Role: {userProfile?.role}</p>
